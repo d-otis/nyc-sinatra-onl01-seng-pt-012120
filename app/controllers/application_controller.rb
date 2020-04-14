0a5d71e5cf461792e6185acf1ff95b4110081c74
d-otis <dan@dan-foley.com>
2020-04-13 20:43:01 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < Sinatra::Base
   set :views, proc { File.join(root, '../views/') }
-  register Sinatra::Twitter::Bootstrap::Assets
+  # register Sinatra::Twitter::Bootstrap::Assets
 
   get '/' do
     erb :"application/index"
--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,52 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+  	@figures = Figure.all
+
+  	erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+  	@figures = Figure.all
+  	@titles = Title.all
+  	@landmarks = Landmark.all
+
+  	erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+  	@figure = Figure.find(params[:id])
+
+  	erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+  	@figure = Figure.find(params[:id])
+  	@titles = Title.all
+  	@landmarks = Landmark.all
+
+  	erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+  	figure = Figure.find(params[:id])
+  	figure.update(params[:figure])
+
+  	figure.titles.create(params[:title]) if !params[:title][:name].empty?
+  	figure.landmarks.create(params[:landmark]) if !params[:landmark][:name].empty?
+
+  	redirect "/figures/#{figure.id}"
+  end
+
+  post '/figures' do
+  	figure = Figure.create(params[:figure])
+
+  	figure.titles.create(params[:title]) if !params[:title][:name].empty?
+
+  	figure.landmarks.create(params[:landmark]) if !params[:landmark][:name].empty?
+
+  	redirect "/figures/#{figure.id}"
+  end
+
 end
